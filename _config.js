c4cdf10f1d09379beafcde74f7afcfadd63d0a22
김응길 <yesroad.dev@gmail.com>
2022-03-22 10:56:09 +0900

    설정 파일 변경

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ module.exports = {
   /** Site MetaData (Required all)*/
   title: `yesroad.Dev`,                           // (* Required)
   description: `프론트엔드 웹 개발 블로그입니다.`, // (* Required)
-  author: `Eunggil Kim`,                         // (* Required)
+  author: `yesroad`,                         // (* Required)
   language: 'ko-KR',                            // (* Required) html lang, ex. 'en' | 'en-US' | 'ko' | 'ko-KR' | ...
   siteUrl: 'https://yesroad.dev',       // (* Required) 
     // ex.'https://junhobaik.github.io'
@@ -15,12 +15,12 @@ module.exports = {
 
   /** Home > Bio information*/
   comment: 'Web Front-end Developer. / javascript, typescript, react ...',
-  name: 'Eunggil Kim',
+  name: 'yesroad',
   company: '',
   location: 'Korea',
   email: 'yesroad.dev@gmail.com',
   website: 'https://yesroad.dev',           // ex.'https://junhobaik.github.io'
-  linkedin: 'https://www.linkedin.com/in/%EC%9D%91%EA%B8%B8-%EA%B9%80-55448a185',                                     // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
+  linkedin: '',                                     // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
   facebook: '',                                     // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
   instagram: '', // ex.'https://www.instagram.com/junhobaik'
   github: 'https://github.com/yesroad',           // ex.'https://github.com/junhobaik'
