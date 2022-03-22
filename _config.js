88dfded2801c1e47bf4ab174a36eef7f1fc887ff
김응길 <yesroad.dev@gmail.com>
2022-03-22 10:25:04 +0900

    ga 추적코드 추가

--- _config.js
+++ _config.js
@@ -31,7 +31,7 @@ module.exports = {
   enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
 
   /** Optional */
-  googleAnalytics: '',                                  // Google Analytics TrackingID. ex.'UA-123456789-0'
+  googleAnalytics: 'G-YNCPGWDSR5',                                  // Google Analytics TrackingID. ex.'UA-123456789-0'
   googleSearchConsole: '', // content value in HTML tag of google search console ownership verification 
   googleAdsenseSlot: '',                                    // Google Adsense Slot. ex.'5214956675'
   googleAdsenseClient: '',                     // Google Adsense Client. ex.'ca-pub-5001380215831339'
